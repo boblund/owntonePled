1e6826c9ba39779af58fb4ac36606df000dd065c
Bob <blund@cablelabs.com>
2023-02-28 16:54:35 -0700

    Initial commit

---
+++ dragdrop.mjs
@@ -0,0 +1,55 @@
+export {allowDrop, dragStart, dragOver, olDrop};
+
+function allowDrop(ev) {
+	ev.preventDefault();
+}
+
+let _dragStartEl;
+
+function dragStart(ev) {
+	ev.target.classList.add('dragging');
+	if(ev.target.parentNode.id != 'Songs' && ev.altKey) {
+		ev.target.remove();
+	} else {
+		ev.dataTransfer.effectAllowed = ev.target.parentNode.id === 'Songs' || ev.shiftKey ? 'copy' : 'move';
+		_dragStartEl = ev.target;
+	}
+}
+
+function dragOver(e) {
+	if(e.target.parentNode.id === 'Songs') {
+		e.target.classList.remove('dragging');
+		return;
+	}
+	if(_dragStartEl.parentNode != e.target.parentNode) return;
+	e.preventDefault();
+	e.target.classList.remove('dragging');
+	if (isBefore(_dragStartEl, e.target)) {
+		e.target.parentNode.insertBefore(_dragStartEl, e.target);
+	} else {
+		e.target.parentNode.insertBefore(_dragStartEl, e.target.nextSibling);
+	}
+}
+
+function olDrop(ev) {
+	_dragStartEl.classList.remove('dragging');
+	(ev.target.nodeName === 'LI' ? ev.target.parentNode : ev.target).setAttribute('changed', 'yes');
+	if(ev.target === _dragStartEl) return;
+	document.querySelector('#savePlBtn').hidden = false;
+	const target = ev.target.nodeName === 'LI' ? ev.target.parentNode : ev.target;
+	if(ev.dataTransfer.effectAllowed === 'copy') {
+		_dragStartEl= _dragStartEl.cloneNode(true);
+		// cloning DOES NOT copy event listeners
+		_dragStartEl.addEventListener('dragover', dragOver);
+		_dragStartEl.addEventListener('dragstart', dragStart);
+	}
+	target.appendChild(_dragStartEl);
+}
+
+function isBefore(el1, el2) {
+	if (el2.parentNode === el1.parentNode)
+		for (var cur = el1.previousSibling; cur && cur.nodeType !== 9; cur = cur.previousSibling)
+			if (cur === el2)
+				return true;
+	return false;
+}
---
+++ songs3.mjs
@@ -0,0 +1,125 @@
+export {addSongs, addPlaylists, newPlaylist, savePlaylists};
+import {allowDrop, dragStart, dragOver, olDrop} from './dragdrop.mjs';
+
+const liAttrs = {draggable: true};
+let playlists = [];
+
+async function addSongs(songsElement) {
+	let songs = [],
+		artists = JSON.parse(await curl(`api/library/artists`)).items
+			.map(e => {const {name, id} = e; return {name, id};})
+			.sort((a,b) => a.name > b.name ? 1 : -1);
+
+	for(const artist of artists) {
+		for(const album of JSON.parse(await curl(`api/library/artists/${artist.id}/albums`)).items.sort((a,b) => a.name > b.name ? 1 : -1)) {
+			JSON.parse(await curl(`api/library/albums/${album.id}/tracks`)).items.forEach(track => {
+				songs.push({
+					trackName: `${track.artist}/${track.album}/${track.title}`,
+					trackPath: track.path
+				});
+			});
+		}
+	}
+
+	songs.forEach((entry) => {
+		songsElement.append(newTrackElement(entry.trackName, entry.trackPath));
+	});
+}
+
+async function addPlaylists(playlistsElement) {
+	playlists = JSON.parse(await curl(`api/library/playlists`)).items
+		.map(e => {const {name, id} = e; return {name, id, tracks: []};})
+		.sort((a,b) => a.name > b.name ? 1 : -1);
+
+	for(const playlist of playlists) {
+		JSON.parse(await curl(`api/library/playlists/${playlist.id}/tracks`)).items.forEach(track => {
+			playlist.tracks.push({
+				trackName: `${track.artist}/${track.album}/${track.title}`,
+				trackPath: track.path
+			});
+		});
+	}
+
+	for(const playlist of playlists) {
+		playlistsElement.append(newPlaylistEntry(playlist));
+	}
+}
+
+function newTrackElement(name, path) {
+	const li = document.createElement('LI');
+	// the event listers must be kept in sync with those in LI drop event handler on node copy
+	li.addEventListener('dragover', dragOver);
+	li.addEventListener('dragstart', dragStart);
+	Object.keys(liAttrs).forEach(attribute => li.setAttribute(attribute, liAttrs[attribute]));
+	li.innerHTML = name;
+	li.setAttribute('path', path);
+	return li;
+}
+
+function newPlaylistEntry(playlist){
+	const details = document.createElement('DETAILS');
+	const summary = document.createElement('SUMMARY');
+	const ul = document.createElement('UL');
+
+	summary.innerHTML = `<b> ${playlist.name} </b>`;
+	details.append(summary);
+
+	ul.setAttribute('id', playlist.name);
+	ul.addEventListener('drop', olDrop);
+	ul.addEventListener('dragover', allowDrop);
+	ul.setAttribute('changed', 'no');
+	details.append(ul);
+	if(playlist.tracks) {
+		playlist.tracks.forEach((entry) => {
+			ul.append(newTrackElement(entry.trackName, entry.trackPath));
+		});
+	}
+	return details;
+}
+
+function newPlaylist(plName) {
+	let idx = playlists.findIndex(playlist => plName < playlist.name);
+	let entry = document.querySelector('#Playlists').childNodes.item(idx);
+	document.querySelector('#Playlists').insertBefore(newPlaylistEntry({name: plName}), entry);
+}
+
+function savePlaylists() {
+	for(const playlist of document.querySelector('#Playlists').childNodes) {
+		if(playlist.childNodes[1].getAttribute('changed') === 'yes') {
+			const m3uName = playlist.childNodes[1].getAttribute('id') + '.m3u';
+			let out = '#EXTM3U\n';
+			for(const li of playlist.childNodes[1].childNodes) {
+				out += li.getAttribute('path') + '\n';
+			}
+			out += '#EXTM3U\n';
+			var textBlob = new Blob([out], {type:'text/plain'});
+			var downloadLink = document.createElement("a");
+			downloadLink.download = m3uName;
+			downloadLink.innerHTML = "Download File";
+			downloadLink.href = window.URL.createObjectURL(textBlob);
+			downloadLink.click();
+			downloadLink = null;
+			textBlob = null;
+			playlist.childNodes[1].setAttribute('changed', 'no');
+		}
+	}
+	document.querySelector('#savePlBtn').hidden = true;
+}
+
+function curl(cmd, method='GET', headers={}, body='') {
+	return new Promise((resolve, reject) => {
+		let href = `http://forked-daapd.local:3689/${cmd}`;
+		const http = new XMLHttpRequest();
+		http.open(method, href);
+		http.send();
+		http.onreadystatechange = function() {
+			if(this.readyState === 4) {
+				if(this.status === 200) {
+					resolve(http.responseText);
+				} else {
+					reject(new Error('Failed to load page, status code: ' + this.status));
+				}
+			}
+		};
+	});
+};
